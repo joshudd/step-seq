db65e3eb99d506105db672a86c22707b4f922820
Josh Dickinson <josh25dickinson@gmail.com>
2024-11-13 23:09:14 -0600

    project initialization

---
+++ step-seq.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=step-seq.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/step-seq.X.production.hex
